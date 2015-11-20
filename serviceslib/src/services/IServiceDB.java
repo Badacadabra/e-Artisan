cd2fe180e3aedb5d8b0817e81549ba707b021866
Badacadabra <baptiste.vannesson@gmail.com>
2015-11-20 16:47:00 +0100

    Documentation, test, et distribution du package « services »

--- serviceslib/src/services/IServiceDB.java
+++ serviceslib/src/services/IServiceDB.java
@@ -1,44 +1,70 @@
-/**
- * 
- */
 package services;
 
-import java.util.Collection;
 import java.util.Map;
 
 /**
- * Interface pour les services échangés.
+ * An interface for database of services. The interface essentially declares CRUD operations.
+ * The key for distinguishing two services from each other is assumed to be a classical id. 
  * @author Macky Dieng
  * @author Baptiste Vannesson
  */
 public interface IServiceDB {
 	
+	// "C" operations
+	
 	/**
-	 * @param s l'instance du service à renvoyer
-	 * @throws Exception
+	 * Adds a service to this database.
+	 * @param s The service to add 
+	 * @throws Exception if a service with the same id already exists in the list or
+     * a database access error occurs
 	 */
 	public void create(Service s) throws Exception;
+	
+	// "R" operations
+	
 	/**
-	 * @param id l'identifiant du service à renvoyer
-	 * @return
-	 * @throws Exception
+	 * Returns the service with the given id.
+	 * @param id The id to search for 
+	 * @return The service with the given id in the list
+	 * @throws Exception if no service with the given id exists in the list or a database access error
+     * occurs
 	 */
 	public Service retrieve(int id) throws Exception;
+	
 	/**
-	 * 
-	 * @return
-	 * @throws Exception
+	 * Returns the list of all services in this database.
+	 * @return The list of all services in this database as an instance of java.util.Map<Integer,Service>.
+	 * @throws Exception if a database access error occurs 
 	 */
 	public Map<Integer,Service> retrieveAll() throws Exception;
+
 	/**
-	 * @param id l'identifiant du service à modifier
-	 * @param s l'instance du service à modifier
-	 * @throws Exception
+     * Decides whether a service with a given id exists in the list.
+     * @return true is the list contains a service with the given id,
+     * false otherwise
+     * @throws Exception if a database access error occurs
+     */
+    public boolean exists(int id) throws Exception;
+	
+	// "U" operations
+	
+	/**
+	 * Updates a service in the database.
+	 * @param id The id of the service to update
+	 * @param s An instance of Service to store in place of the existing one
+	 * @throws Exception if no service is currently associated to the given id,
+     * or the new instance has an id which already exists in the database, or a
+     * database access error occurs
 	 */
 	public void update(int id, Service s) throws Exception;
+	
+	// "D" operations
+	
 	/**
-	 * @param id l'identifiant du service à supprimer
-	 * @throws Exception
+	 * Removes the service with a given id from this database.
+	 * @param id The id of the service to remove
+	 * @throws Exception if no service is currently associated to the given id
+     * or a database access error occurs
 	 */
 	public void delete(int id) throws Exception;
 	
